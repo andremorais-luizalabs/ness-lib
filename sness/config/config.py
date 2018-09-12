06cfe4a65af5f8a6dedda5daed8c77f9d692428d
Andre Sasaki <aj_sasaki@hotmail.com>
2018-09-12 10:02:25 -0300

    added buckets dict'

--- sness/config/config.py
+++ sness/config/config.py
@@ -2,3 +2,26 @@ ZONE = "global"
 REGION = "us-east1-b"
 PROJECT = "maga-bigdata"
 FOO = "xBC7vDcmvqD49qif"
+
+BUCKETS = {
+    "zone": {
+        "transient":
+            {
+                "atena": "prd-lake-transient-atena",
+                "olimpo": "prd-lake-transient-olimpo",
+                "p52": "prd-lake-transient-p52",
+                "precifica": "prd-lake-transient-precifica",
+                "stewie": "prd-lake-transient-stewie",
+                "wifi": "prd-lake-transient-wifi",
+            },
+        "raw":{
+                "atena": "prd-lake-raw-atena",
+                "olimpo": "prd-lake-raw-olimpo",
+                "precifica": "prd-lake-raw-precifica",
+                "stewie": "prd-lake-raw-stewie"
+        },
+        "trusted": {
+                "atena": "prd-lake-trusted-atena"
+        }
+    }
+}
