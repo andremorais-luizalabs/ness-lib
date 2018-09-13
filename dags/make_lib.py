fb6dba347d5833531d134a0ac8aef1221a4438c1
Andre Sasaki <aj_sasaki@hotmail.com>
2018-09-13 11:27:00 -0300

    updated dag

--- dags/make_lib.py
+++ dags/make_lib.py
@@ -18,22 +18,16 @@ dag = DAG('make_lib', default_args=default_args)
 inicio = DummyOperator(task_id='Inicio', dag=dag)
 fim = DummyOperator(task_id='Fim', dag=dag)
 
-pullMake = BashOperator(
-    task_id = "pull_and_make_lib",
-    bash_command = "git clone https://github.com/luizalabs/ness-lib.git && cd ness-lib",
-    dag=dag)
-
 uploadLibGcp= BashOperator(
     task_id = "upload_Lib_Airflow",
-    bash_command = "ls /tmp",
+    bash_command = "git clone https://github.com/luizalabs/ness-lib.git && cd ness-lib && gsutil -m cp -r sness/ gs://us-east1-ness-maestro-782d5135-bucket/dags/",
     dag=dag)
 
 uploadDagsGcp= BashOperator(
     task_id = "upload_dags_Airflow",
-    bash_command = "gsutil -m cp -r dags/ gs://us-east1-ness-maestro-782d5135-bucket/dags/",
+    bash_command = "git clone https://github.com/luizalabs/ness-lib.git && cd ness-lib && gsutil -m cp -r dags/ gs://us-east1-ness-maestro-782d5135-bucket/dags/",
     dag=dag)
 
 
-inicio.set_downstream(pullMake)
-pullMake.set_downstream([uploadDagsGcp, uploadLibGcp])
+inicio.set_downstream([uploadDagsGcp, uploadLibGcp])
 fim.set_upstream([uploadDagsGcp, uploadLibGcp])
