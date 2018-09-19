9a7b0fca6038ac798c0dc556f80a11e484f8852e
Andre Sasaki <aj_sasaki@hotmail.com>
2018-09-19 15:28:25 -0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -19,8 +19,7 @@ setup(
     keywords="spark gcp etl",
     install_requires=[
         "google-api-python-client==1.7.4", "google-api-core==1.3.0", "google-cloud==0.34.0",
-        "google-cloud-storage==1.11.0", "google-cloud-bigquery==1.5.0", "pyjarowinkler==1.8",
-        "pandas==0.23.4", "scikit-learn==0.19.2", "fire==0.1.3"
+        "google-cloud-storage==1.11.0", "google-cloud-bigquery==1.5.0", "pandas==0.23.4", "scikit-learn==0.19.2"
     ],
     packages=[
         'sness', 'sness.gcloud', 'sness.utils',
