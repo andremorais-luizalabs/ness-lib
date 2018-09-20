08f5d1b2e216e89a08b640d9d82eec72e073ff89
Andre Sasaki <aj_sasaki@hotmail.com>
2018-09-20 14:42:58 -0300

    added new json method to sness/metadata

--- sness/config/metadata.py
+++ sness/config/metadata.py
@@ -1,8 +1,8 @@
 INSERT_METADATA = '''INSERT INTO dataset_catalog (zone, namespace, dataset, 
 columns, last_update, file_format, bucket, source_path, destination_path, 
-partition_columns) VALUES ({zone}, {namespace}, {dataset}, '{columns}', 
-{last_update}, {file_format}, {bucket}, {source_path}, 
-{destination_path}, {partition_columns});'''
+partition_columns) VALUES ('{zone}', '{namespace}', '{dataset}', '{columns}', 
+to_timestamp('{last_update}', 'dd-mm-yyyy hh24:mi:ss'), '{file_format}', '{bucket}', '{source_path}', 
+'{destination_path}', '{partition_columns}');'''
 
 GET_METADATA = '''SELECT * from dataset_catalog where zone = {zone} and namespace = {namespace} 
 and dataset = {dataset} and bucket = {bucket} ORDER BY last_update DESC;'''
--- sness/metadata.py
+++ sness/metadata.py
@@ -9,6 +9,7 @@ def save_metadata(df, zone, namespace, dataset, partition_columns=None):
     file_format = "PARQUET"
     destination_bucket = BUCKETS.get(zone).get(namespace)
     destination_path = "{}/{}".format(dataset, last_update.strftime("%Y%m%d%H%M%S"))
+    partition_columns = ','.join(partition_columns) if partition_columns else partition_columns
     query = INSERT_METADATA.format(zone=zone, namespace=namespace, dataset=dataset,
                                    columns=columns, last_update=last_update,
                                    file_format=file_format, bucket=destination_bucket,
